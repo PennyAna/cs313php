0dd4b3e864e3d18fb48c69abb3eab50ca8e9bdf5
PennyAna <law07006@byui.edu>
2020-09-22 21:48:35 -0600

    try try again links

--- web/php/home.php
+++ web/php/home.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title></title>
-<link rel="stylesheet" href="css/wk2.css" type="text/css">
+<link rel="stylesheet" href="../css/wk2.css" type="text/css">
 </head>
 <body>
 <div class="header">
