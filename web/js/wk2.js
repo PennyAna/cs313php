20a29fc28ac785182e05f350f683a839a00b5f60
PennyAna <law07006@byui.edu>
2020-09-22 21:22:40 -0600

    basic setup wk2, main home

---
+++ web/js/wk2.js
@@ -0,0 +1,4 @@
+function googleTranslateElementInit() {
+	new google.translate.TranslateElement({pageLanguage: 'en', 
+		layout:google.translate.TranslateElement.InlineLayout.SIMPLE}, 
+		'google_translate_element');}
