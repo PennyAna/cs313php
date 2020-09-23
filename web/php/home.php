2554d76f4c7617b7b90cea8b7b0abd99663dd8ce
PennyAna <law07006@byui.edu>
2020-09-22 22:00:46 -0600

    css me

--- web/php/home.php
+++ web/php/home.php
@@ -8,7 +8,6 @@
 </head>
 <body>
 <div class="header">
-
 <br></div>
 <div id="phpMe">
 <?php 
@@ -24,62 +23,52 @@ if ($t < 10) {
 }
 ?>
 </div><br>
-<div class="row">
-<div class="col-3 menu">
-<br>
-<a id="pageTwoLink" href="../html/wk2.html">Assignments</a>
-<br><br>
-<p id="text"></p>
-<div id="">
-<form>
-<label id=""></label><br>
-<fieldset>		
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <input type="radio" name="" value=""><br>
-  <br>
- <input id="submitMe" type="submit" name="submit" value="submit">
-  <input id="resetMe" type="reset" name="reset" value="reset">
- </fieldset>
-</form>
-<br><br>
-<img id="" src="" alt=""><br>
-</div></div>
-<br><br>
-
-<div id="" class="col-9 col-s-12">
-<button type="button" class="collapsible"></button>
-<div class="">
-<p>
-</p></div><br>
-<button type="button" class="collapsible"></button>
-<div class="">
-<p>
-</p></div><br>
-<button type="button" class="collapsible"></button>
-<div class="">
-<p>
-</p></div><br>
-<button type="button" class="collapsible"></button>
-<div class="">
-<p>
-</p></div><br>
-<button type="button" class="collapsible"></button>
-<div class="">
-<p>
-<p></div><br></div></div>
+<div class="container-fluid">
+	<div class="row content">
+		<div class="col-sm-3 sidenav">
+			<h4>Title</h4>
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="../php/home.php">Home</a></li>
+				<li class="active"><a href="./wk2.html">Assignments</a></li>
+			</ul><br>
+			<div class="input-group">
+				<input type="text" class="form-control" placeholder="Search Site...">
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="button">
+						<span class="glyphicon glyphicon-search"></span>
+					</button>
+				</span>
+			</div>
+		</div>
+		<div class="col-sm-9">
+			<h4><small>POSTS</small></h4>
+			<hr>
+			<h2>Blah Blah</h2>
+			<h5>Post post</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<br><br>
+										<br>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<footer class="container-fluid">
+			<div class="footer"><br>Text Source: </div>
+			</footer>
 <script src="js/wk2.js"></script>
-<div class="footer"><br>Text Source: </div>
 </body>
 </html>
