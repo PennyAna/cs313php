ac0fb9ab423ba79304230ad69c2ca09c4fad225e
PennyAna <law07006@byui.edu>
2020-09-26 21:49:53 -0600

    format blog post

--- web/php/home.php
+++ web/php/home.php
@@ -16,7 +16,7 @@
 			<div class="col-sm-3 sidenav">
 				<div id="sidenav">
 					<div id="animeMe">
-						<img src="../images/animeMe.jpg" alt="Girl with Glasses" class="img-sm-rounded">
+						<img src="../images/animeMe.jpg" alt="Girl with Glasses" class="img-fluid">
 					</div>
 						<h3>Title</h3>
 					<ul class="nav nav-pills nav-stacked">
