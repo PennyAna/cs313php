1d272eea9550a68bd11f71dd8701563047cc2d84
PennyAna <law07006@byui.edu>
2020-09-28 00:21:41 -0600

    setting up link and pages for shopping cart

--- web/php/shop_checkout.php
+++ web/php/shop_checkout.php
@@ -1,15 +1,14 @@
-/*The checkout page should ask the user for the different components of their address. (No credit card or other purchase information is collected, only an address.)
+<!--The checkout page should ask the user for the different components of their address. (No credit card or other purchase information is collected, only an address.)
 
-It should have the option to complete the purchase or return to the cart.
-*/
+It should have the option to complete the purchase or return to the cart.-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Shop</title>
 </head>
 <body>
-    
+    <div>I'm an empty shop!</div>
 </body>
 </html>
