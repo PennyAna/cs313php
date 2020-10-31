c8b1a9d3c510cfce6b995204bd15fa811a9fad71
PennyAna <law07006@byui.edu>
2020-10-30 23:11:31 -0600

    team5 practice

--- web/php/dbConnect.php
+++ web/php/dbConnect.php
@@ -1,14 +1,7 @@
 function get_db() {
    
     try {
-        $db = parse_url(getenv("DATABASE_URL"));
-        $pdo = newPDO("pgsql:" . sprintf(
-        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-        $db["host"], 
-        $db["port"], 
-        $db["user"], 
-        $db["pass"], 
-        ltrim($db["path"],"/") 
+        $db = parse_url(getenv("dbname=ec2-3-214-46-194.compute-1.amazonaws.com port=5432 user=gpyxtqyzcuqcjk password=60bc1426f9ccbafd71126491edc3d9c56c89e00e42febcd843ffc7eccb901049 sslmode=require"));
     ));
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
