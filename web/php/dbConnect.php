81a5c03e805c4d0086d87a0f983a8dc4876384ac
PennyAna <law07006@byui.edu>
2020-10-30 23:23:07 -0600

    practice5

--- web/php/dbConnect.php
+++ web/php/dbConnect.php
@@ -1,13 +1,22 @@
 function get_db() {
    
-    try {
-        $db = parse_url(getenv("dbname=ec2-3-214-46-194.compute-1.amazonaws.com port=5432 user=gpyxtqyzcuqcjk password=60bc1426f9ccbafd71126491edc3d9c56c89e00e42febcd843ffc7eccb901049 sslmode=require"));
-    ));
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $ex) {
-        echo "Error connecting to DB. Details: $ex";
-        die();
-    }
+    Connecting to Postgres from PHP
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+  $dbOpts = parse_url($dbUrl);
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
     return $db;
 }
