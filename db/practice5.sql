1a26e1ed8967bd2a6140a834b12a6267c0936a15
PennyAna <law07006@byui.edu>
2020-10-30 22:44:26 -0600

    team5 practice

--- db/practice5.sql
+++ db/practice5.sql
@@ -2,15 +2,16 @@
 CREATE TABLE scripture (
     id SERIAL PRIMARY KEY NOT NULL, 
     book VARCHAR(80) NOT NULL, 
-    chapter INT NOT NULL, 
+    chapter INT NOT NULL,
+    verse INT NOT NULL,  
     content VARCHAR(4000) NOT NULL
 );
 #insert data into new TABLE
-INSERT INTO scripture(book, chapter, verse, content)
-VALUES('John', 1, 4, 'And the light shineth in darkness; and the darkness comprehended it not.');
-INSERT INTO scripture(book, chapter, verse, content)
-VALUES('Doctrine and Covenants', 93, 28, 'He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things.');
-INSERT INTO scripture(book, chapter, verse, content)
-VALUES('Mosiah', 16, 9, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yeah, and also a life which is endless, that there can be no more death.');
-INSERT INTO scripture(book, chapter, verse, content)
-VALUES('Doctrine and Covenants', 88, 49, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall ocme when you shall comprehend even God, being quickened in him and by him.');
+INSERT INTO scripture(book, chapter, content, verse)
+VALUES('John', 1, 'And the light shineth in darkness; and the darkness comprehended it not.', 4);
+INSERT INTO scripture(book, chapter, content, verse)
+VALUES('Doctrine and Covenants', 93, 'He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things.', 28);
+INSERT INTO scripture(book, chapter, content, verse)
+VALUES('Mosiah', 16, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yeah, and also a life which is endless, that there can be no more death.',  9);
+INSERT INTO scripture(book, chapter, content, verse)
+VALUES('Doctrine and Covenants', 88, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall ocme when you shall comprehend even God, being quickened in him and by him.', 49);
