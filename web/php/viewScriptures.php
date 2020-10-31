d8802c1e99cc12cef64239215c2ca41b27b0956d
PennyAna <law07006@byui.edu>
2020-10-30 23:52:02 -0600

    practice5

--- web/php/viewScriptures.php
+++ web/php/viewScriptures.php
@@ -11,6 +11,7 @@
 <?php
     require "dbConnect.php";
     $db = get_db();
+    ?>
     $statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
     $statement->execute();
     while($row = $statement->fetch(PDO::FETCH_ASSOC)){
@@ -20,7 +21,6 @@
         $content = $row['content'];
         echo "<p><strong>$book $chapter:$verse</strong> - \'$content\'</p>";
     }
-?>
 </div>
 </body>
 </html>
