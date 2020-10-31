9a943f7631314168503c54cd956658a7ded90cae
PennyAna <law07006@byui.edu>
2020-10-30 22:48:06 -0600

    team5 practice

---
+++ web/php/dbConnect.php
@@ -0,0 +1,25 @@
+function get_db() {
+    $db - NULL;
+    try {
+        //default Heroku Postgres configuration URL
+        $dbURL = getenv('DATABASE_URL');
+        }
+        //get var parts of DB connect from URL
+        $dbopts = parse_url($dUrl);
+        $dbHost = $dbopts["host"];
+        $dbPort = dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"], '/');
+        //create PDO connection_aborted
+        $db = new PDO(pgsql:host=$dbHost;port=$dbPort;dbname=$dbName, $dbUser, $dbPassword);
+        //makes PDO give exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $ex) {
+        //do not do this in production
+        echo "Error connecting to DB. Details: $ex";
+        die();
+    }
+    return $db;
+}
---
+++ web/php/viewScriptures.php
@@ -0,0 +1,28 @@
+<?php
+require "dbConnect.php";
+$db = get_db();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scripture List</title>
+</head>
+<body>
+<h1>Scripture Resources</h1>   
+<div>
+<?php
+    $statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
+    $statement->execute();
+    while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+        $book = $row['book'];
+        $chapter = $row['chapter'];
+        $verse = $row['verse'];
+        $content = $row['content'];
+        echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"</p>";
+    }
+?>
+</div>
+</body>
+</html>
